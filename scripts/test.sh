fa4ee4db541c0ed2e1f33cc42e84cccfcbf420ae
joishbader <joishbader@gmail.com>
2018-04-21 15:12:24 +0800

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 CUDA_VISIBLE_DEVICES="3" \
   python trainval_net.py \
-       --data_dir=/home/jbr/storage-jbr/RLObjectDetection/data/coco/images/val2014 \
-       --anno_file=/home/jbr/storage-jbr/RLObjectDetection/data/coco/annotations/instances_minival2014.json \
-       --labels_file=/home/jbr/storage-jbr/RLObjectDetection/data/coco/labels/minival2014_action_01.json \
+       --data_dir=data/coco/images/val2014 \
+       --anno_file=data/coco/annotations/instances_minival2014.json \
+       --labels_file=data/coco/labels/minival2014_action_01.json \
        --resume=snapshot/epoch_1.pth \
        --batch-size=1 \
        --evaluate \
-       2>&1 | tee test.log 
+       2>&1 | tee log/test.log 
